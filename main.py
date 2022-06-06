52f77a10f026df080a4d7e4cf0d96a7c59a72d9e
Theo <95878514+TheoW03@users.noreply.github.com>
2022-06-06 16:39:39 -0400

    init commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+
+
+WIN_HEIGHT,WIN_WIDTH = 400,400
+WIN = pygame.display.set_mode((WIN_HEIGHT, WIN_WIDTH))
+
+
+def main():
+    close = True
+
+    while close:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                close = False
+    pygame.quit()
+
+
+if __name__ == '__main__':
+     main()
+
+    
