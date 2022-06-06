0f9bf6b15baaaac56df933bd4cb098a1dc59715a
Theo <95878514+TheoW03@users.noreply.github.com>
2022-06-06 17:30:16 -0400

    Added OG sprites

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,16 +1,34 @@
 import pygame
 from main_Character import mainChar
+from pygame.locals import *
+
+import os
 
 WIN_HEIGHT, WIN_WIDTH = 400, 400
 WIN = pygame.display.set_mode((WIN_HEIGHT, WIN_WIDTH))
 
 
+# sprites
+player = pygame.image.load(os.path.join('Sprites', 'sprite1.PNG'))
+playerObj = pygame.transform.scale(player, (50, 40))
+sRect = pygame.Rect(100, 100, 50, 40)
+m = mainChar(sRect)
+
+
+def draw_sprites():
+    global sRect, m
+    color = (50.2, 50.2, 50.2)
+    WIN.fill(color)
+    WIN.blit(playerObj, m.getVector())
+    pygame.display.update()
 def main():
+    global sRect,m
     close = True
     while close:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 close = False
+        draw_sprites()
     pygame.quit()
 
 
--- Scripts/main_Character.py
+++ Scripts/main_Character.py
@@ -1,3 +1,5 @@
+# from cmath import rect
+# from curses.textpad import rectangle
 import pygame
 
 
@@ -5,4 +7,19 @@ class mainChar:
     def __init__ (self,rectangle):
         self.rectangle = rectangle
     def move_up(self):
-        ...
+        self.rectangle.y += 1
+    def move_down(self):
+        self.rectangle.y -= 1
+    def move_right(self):
+        self.rectangle.x += 1
+    def move_left(self):
+        self.rectangle.x  -= 1
+    def getRectangle(self):
+        return self.rectangle
+    def getVector(self):
+        return ((self.rectangle.x, self.rectangle.y))
+    def getX(self):
+        return self.rectangle.x
+    def getY(self):
+        return self.rectangle.y
+    
