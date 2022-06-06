554a8525e262d11dd6a60c5d3f7dd25f37d837e9
Theo <95878514+TheoW03@users.noreply.github.com>
2022-06-06 16:48:39 -0400

    Added window

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,13 +1,12 @@
 import pygame
+from main_Character import mainChar
 
-
-WIN_HEIGHT,WIN_WIDTH = 400,400
+WIN_HEIGHT, WIN_WIDTH = 400, 400
 WIN = pygame.display.set_mode((WIN_HEIGHT, WIN_WIDTH))
 
 
 def main():
     close = True
-
     while close:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -16,6 +15,4 @@ def main():
 
 
 if __name__ == '__main__':
-     main()
-
-    
+    main()
---
+++ Scripts/main_Character.py
@@ -0,0 +1,8 @@
+import pygame
+
+
+class mainChar:
+    def __init__ (self,rectangle):
+        self.rectangle = rectangle
+    def move_up(self):
+        ...
